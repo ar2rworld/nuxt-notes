4fb51e37f9d815350df7418449cfd57034fe95c5
Artur <ar2r.world@gmail.com>
2025-05-07 20:30:06 +0500

    fix title and icons space

--- components/NoteCard.vue
+++ components/NoteCard.vue
@@ -1,22 +1,24 @@
 <template>
-  <li class="p-4 bg-white rounded-lg shadow transition-shadow hover:shadow-md mx-auto relative" style="max-width: 500px;">
-    <div class="absolute top-2 right-2 flex gap-2">
-      <NuxtLink :to="`/note/${id}`" class="p-2 text-gray-500 hover:text-blue-500 transition-colors">
-        <Icon name="uil:pen" style="color: black" />
+  <li class="p-4 bg-white rounded-lg shadow transition-shadow hover:shadow-md mx-auto relative">
+    <div class="absolute top-4 right-4 flex gap-2">
+      <NuxtLink :to="`/note/${id}`" class="p-1.5 text-gray-500 hover:text-blue-500 transition-colors">
+        <Icon name="uil:pen" size="1.2em" />
       </NuxtLink>
       <button 
-        class="p-2 text-gray-500 hover:text-red-500 transition-colors"
+        class="p-1.5 text-gray-500 hover:text-red-500 transition-colors"
         @click="handleDelete" 
       >
-        <Icon name="uil:trash" style="color: black" />
+        <Icon name="uil:trash" size="1.2em" />
       </button>
     </div>
-    <h3 class="text-lg font-semibold text-gray-800 mb-1 truncate pr-8">
-      {{ notename }}
-    </h3>
-    <p class="text-sm text-gray-600">
-      {{ text }}
-    </p>
+    <div class="pr-16">
+      <h3 class="text-lg font-semibold text-gray-800 mb-2 break-words">
+        {{ notename }}
+      </h3>
+      <p class="text-sm text-gray-600">
+        {{ text }}
+      </p>
+    </div>
   </li>
 </template>
 
